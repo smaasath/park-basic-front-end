18d8d99b5d081af5e40ff6cfab8c606d44868457
R.D.K.S. Kumara <134190302+sanjayakasun@users.noreply.github.com>
2024-01-26 21:30:25 +0530

    parking_time slots

--- src/pages/HomeFlow/BookingMain/BookingSlot/BookingSlot.js
+++ src/pages/HomeFlow/BookingMain/BookingSlot/BookingSlot.js
@@ -2,6 +2,9 @@ import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import './BookingSlot.css'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import NavBar from '../../../../components/NavBar/NavBar'
 import Footer from '../../../../components/Footer/Footer'
 
@@ -12,35 +15,34 @@ export default function BookingSlot() {
     <div className='body'>
          <NavBar></NavBar>
         <div className='form'>
-        <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Slot ID</Form.Label>
-        <Form.Control type="text" placeholder="Slot ID" />
-      </Form.Group>
+      <Form>
+      <h6>Select The Time Slot</h6>
       <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>User ID</Form.Label>
-        <Form.Control type="text" placeholder="User ID" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-      <Form.Label>Select The Time Slot</Form.Label>
-      <Form.Select aria-label="Default select example">
-      <option>Select The Time Slot</option>
-      <option value="1">6.00 a.m - 8.00 a.m</option>
-      <option value="2">8.00 a.m - 10.00 a.m</option>
-      <option value="3">10.00 a.m - 12.00 p.m</option>
-      <option value="4">12.00 p.m - 2.00 p.m</option>
-      <option value="5">2.00 p.m - 4.00 p.m</option>
-      <option value="6">4.00 p.m - 6.00 p.m</option>
-      <option value="4">6.00 p.m - 8.00 p.m</option>
-      <option value="5">8.00 p.m - 10.00 p.m</option>
-      <option value="6">10.00 p.m - 12.00 a.m</option>
-    </Form.Select>
+      <Container>
+      <Row className='slot_row'>
+        <Col lg={4} md={3} xs={12} className='mt-2'><Button variant="light">6.00 a.m - 8.00 a.m</Button> </Col>
+        <Col lg={4} md={3} xs={12} className='mt-2'><Button variant="light">8.00 a.m -10.00 a.m</Button> </Col>
+        <Col lg={4} md={3} xs={12} className='mt-2'><Button variant="light">10.00 a.m -12.00 p.m</Button> </Col>
+      </Row>
+      <Row className='slot_row'>
+        <Col lg={4} md={3} xs={12} className='mt-2'><Button variant="light">12.00 p.m - 2.00 p.m</Button></Col>
+        <Col lg={4} md={3} xs={12} className='mt-2'><Button variant="light">2.00 p.m - 4.00 p.m</Button>  </Col>
+        <Col lg={4} md={3} xs={12} className='mt-2'><Button variant="light">4.00 p.m - 6.00 p.m</Button> </Col>
+      </Row>
+      <Row className='slot_row'>
+        <Col lg={4} md={3} xs={12} className='mt-2'><Button variant="light">6.00 p.m - 8.00 p.m</Button> </Col>
+        <Col lg={4} md={3} xs={12} className='mt-2'><Button variant="light">8.00p.m -10.00 p.m</Button>  </Col>
+        <Col lg={4} md={3} xs={12} className='mt-2'><Button variant="light">10.00 p.m-12.00 a.m</Button>  </Col>
+      </Row>
+
+    </Container>
     </Form.Group>
-      <Button variant="primary" type="submit">
-        Book
-      </Button>
     </Form>
+    
         </div>
+        <br/>
+        <br/>
+        <br/>
         <Footer></Footer>
     </div>
   )
