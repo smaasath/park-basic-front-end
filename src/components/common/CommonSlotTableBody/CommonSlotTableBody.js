f214252a82ed566cc3b485d8b3dcd6b6e6bb3d57
Aasath <aasadh2000@gmail.com>
2024-01-29 19:34:07 +0530

    slot delete

--- src/components/common/CommonSlotTableBody/CommonSlotTableBody.js
+++ src/components/common/CommonSlotTableBody/CommonSlotTableBody.js
@@ -2,13 +2,19 @@ import React, { useEffect, useState } from 'react'
 import AddSlotModel from '../../Models/AddSlotModel/AddSlotModel';
 import editIcon from "../../../assests/pictures/edit.png";
 import deleteIcon from "../../../assests/pictures/delete.png";
+import DeleteModel from '../../Models/DeleteModel/DeleteModel';
 
-
-const CommonSlotTableBody = ({ item, seteditModalShow}) => {
+const CommonSlotTableBody = ({ item, editModalShows, deleteModalShows, seteditModalShow, setdeleteModalShows }) => {
     const [editModalShow, seteditModalShows] = useState(false);
+    const [deleteModalShow, setdeleteModalShow] = useState(false);
     const handleEditClick = () => {
         seteditModalShows(true);
     };
+
+    const handleDeleteClick = () => {
+        setdeleteModalShow(true);
+    };
+
     return (
         <>
             <tr>
@@ -20,7 +26,7 @@ const CommonSlotTableBody = ({ item, seteditModalShow}) => {
                             <img onClick={handleEditClick} src={editIcon} style={{ width: 20, height: 20 }} />
                         </div>
                         <div>
-                            <img src={deleteIcon} style={{ width: 20, height: 20 }} />
+                            <img onClick={handleDeleteClick} src={deleteIcon} style={{ width: 20, height: 20 }} />
                         </div>
                     </div>
                 </td>
@@ -28,10 +34,19 @@ const CommonSlotTableBody = ({ item, seteditModalShow}) => {
             <AddSlotModel
                 show={editModalShow}
                 onHide={() => {
-                    seteditModalShow(false)
+                    seteditModalShow(editModalShows + 1)
                     seteditModalShows(false);
                 }}
                 item={item}
+            />
+
+            <DeleteModel
+                show={deleteModalShow}
+                onHide={() => {
+                    setdeleteModalShows(deleteModalShows + 1)
+                    setdeleteModalShow(false);
+                }}
+                item={item.id}
             />
         </>
     )
